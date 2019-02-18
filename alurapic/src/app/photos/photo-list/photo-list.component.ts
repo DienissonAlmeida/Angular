d69356ac7471f0973cb269f039ecadbff7adf665
Dienisson Almeida <dienisson.chinelatto@ndd.com.br>
2019-02-18 17:11:53 -0300

    -> Implementações do componente para listar fotos. -> Adequação dos dados recebidos pelo compoenente. -> Quando a abordagem OnInit não é suficiente. -> A interface OnChanges, e como interagir como SimpleChanges.

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoService } from '../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
+import { Photo } from '../photo/photo';
 
 @Component({
-  selector: 'app-photo-list',
+  selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
 export class PhotoListComponent implements OnInit {
 
-  photos: Array<Object> = [];
+  photos: Array<Photo> = [];
 
   constructor(private photoService: PhotoService, private activatedRoute : ActivatedRoute) { }
 
